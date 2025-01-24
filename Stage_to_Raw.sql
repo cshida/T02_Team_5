834751799333a145caeaacc89b003280547fccb5
Jacob s10241569 <S10241569@connect.np.edu.sg>
2025-01-24 18:15:54 +0800

    meteadata

--- Stage_to_Raw.sql
+++ Stage_to_Raw.sql
@@ -40,3 +40,24 @@ FILE_FORMAT = (
 )
 ON_ERROR=ABORT_STATEMENT;
 -- For more details, see: https://docs.snowflake.com/en/sql-reference/sql/copy-into-table
+TRUNCATE TABLE "TRUSTBANKDATA"."RAW"."LAGOS_TRANSACTIONS";
+-- For more details, see: https://docs.snowflake.com/en/sql-reference/sql/truncate-table
+COPY INTO "TRUSTBANKDATA"."RAW"."LAGOS_TRANSACTIONS"
+FROM (
+    SELECT $1, $2, $3, $4, $5, $6, $7
+    FROM '@"TRUSTBANKDATA"."RAW"."NEW_CSV_STAGE"'
+)
+FILES = ('lagos_transactions.csv.gz')
+FILE_FORMAT = (
+    TYPE=CSV,
+    SKIP_HEADER=-1,
+    FIELD_DELIMITER=',',
+    TRIM_SPACE=TRUE,
+    FIELD_OPTIONALLY_ENCLOSED_BY='"',
+    REPLACE_INVALID_CHARACTERS=TRUE,
+    DATE_FORMAT=AUTO,
+    TIME_FORMAT=AUTO,
+    TIMESTAMP_FORMAT=AUTO
+)
+ON_ERROR=ABORT_STATEMENT;
+-- For more details, see: https://docs.snowflake.com/en/sql-reference/sql/copy-into-table
