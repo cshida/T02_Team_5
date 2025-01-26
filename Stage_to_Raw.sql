6046763723d4fa6039ae7dcf4fefe9e6a89c445d
Jacob s10241569 <S10241569@connect.np.edu.sg>
2025-01-26 15:22:37 +0800

    test

--- Stage_to_Raw.sql
+++ Stage_to_Raw.sql
@@ -68,6 +68,27 @@ BEGIN
     ON_ERROR=ABORT_STATEMENT;
 
     RETURN 'Data load for all tables completed successfully.';
+    TRUNCATE TABLE "TRUSTBANKDATA"."RAW"."BRANCH_PERFORMANCE";
+
+    COPY INTO "TRUSTBANKDATA"."RAW"."BRANCH_PERFORMANCE"
+    FROM (
+        SELECT $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $17, $18
+        FROM '@"TRUSTBANKDATA"."RAW"."NEW_CSV_STAGE"'
+    )
+        FILES = ('Branch_performance.csv.gz')
+        FILE_FORMAT = (
+            TYPE=CSV,
+            SKIP_HEADER=-1,
+            FIELD_DELIMITER=',',
+            TRIM_SPACE=TRUE,
+            FIELD_OPTIONALLY_ENCLOSED_BY='"',
+            REPLACE_INVALID_CHARACTERS=TRUE,
+            DATE_FORMAT=AUTO,
+            TIME_FORMAT=AUTO,
+            TIMESTAMP_FORMAT=AUTO
+    )
+    ON_ERROR=ABORT_STATEMENT;
+
 END;
 $$;
 
