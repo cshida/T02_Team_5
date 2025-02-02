aa7f77a2b927e5a80a129f03232e9bdc082b80d3
Jacob s10241569 <S10241569@connect.np.edu.sg>
2025-02-02 19:07:12 +0800

    updated file types and made sure stremas were resumed

--- Harmonized schema/Tasks.sql
+++ Harmonized schema/Tasks.sql
@@ -1,30 +1,44 @@
--- Streams
+-- Tasks Below
 
-CREATE OR REPLACE STREAM trustbankdata_harmonized_transactions_stream
-  ON TABLE TRUSTBANKDATA.HARMONIZED.TRANSACTIONS;
+CREATE OR REPLACE TASK refresh_materialized_view_task
+  WAREHOUSE = 'trustbank_reporting_warehouse'
+  SCHEDULE = 'USING CRON 0 1 * * * UTC'
+AS
+  select * from TRUSTBANKDATA.HARMONIZED.VW_SALES_SUMMARY;
+ALTER TASK TRUSTBANKDATA.HARMONIZED.refresh_materialized_view_task RESUME;
 
-CREATE OR REPLACE STREAM trustbankdata_harmonized_investments_stream
-  ON TABLE TRUSTBANKDATA.HARMONIZED.INVESTMENTS;
 
-CREATE OR REPLACE STREAM trustbankdata_harmonized_loan_stream
-  ON TABLE TRUSTBANKDATA.HARMONIZED.LOAN;
 
 CREATE OR REPLACE TASK monitor_stream_health_transaction
   WAREHOUSE = 'trustbank_ETL_Warehouse'
   SCHEDULE = '15 MINUTE'
 AS
   INSERT INTO system_health_logs SELECT 'Stream Check', CURRENT_TIMESTAMP(), COUNT(*) FROM trustbankdata_harmonized_transactions_stream;
+ALTER TASK TRUSTBANKDATA.HARMONIZED.monitor_stream_health_transaction RESUME;
+
+
 
 CREATE OR REPLACE TASK monitor_stream_health_investment
   WAREHOUSE = 'trustbank_ETL_Warehouse'
   SCHEDULE = '15 MINUTE'
 AS
   INSERT INTO system_health_logs SELECT 'Stream Check', CURRENT_TIMESTAMP(), COUNT(*) FROM trustbankdata_harmonized_investments_stream;
+ALTER TASK TRUSTBANKDATA.HARMONIZED.monitor_stream_health_investment RESUME;
+
+
 
 CREATE OR REPLACE TASK monitor_stream_health_loan
   WAREHOUSE = 'trustbank_ETL_Warehouse'
   SCHEDULE = '15 MINUTE'
 AS
   INSERT INTO system_health_logs SELECT 'Stream Check', CURRENT_TIMESTAMP(), COUNT(*) FROM trustbankdata_harmonized_loan_stream;
+ALTER TASK TRUSTBANKDATA.HARMONIZED.monitor_stream_health_loan RESUME;
 
 
+
+SELECT *
+FROM TABLE(INFORMATION_SCHEMA.TASK_HISTORY(
+    TASK_NAME=>'REFRESH_DAILY_SALES_SUMMARY_TASK',
+    SCHEDULED_TIME_RANGE_START=>DATEADD('days', -7, CURRENT_TIMESTAMP()),
+    SCHEDULED_TIME_RANGE_END=>CURRENT_TIMESTAMP()
+    ));
