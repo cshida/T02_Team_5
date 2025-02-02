8a571f84bbc48a375a6285079a9fd1c3a9004f1e
maximchongADO <maximchongzhss1e2@gmail.com>
2025-02-02 20:09:28 +0800

    Create Database Infrastructure Setup.sql

---
+++ Database Administration /Database Infrastructure Setup.sql
@@ -0,0 +1,7 @@
+-- Creating Schemas--
+
+CREATE or REPLACE Schema TRUSTBANKDaTa.harmonized;
+CREATE OR REPLACE SCHEMA trustbankdata.error_logs;
+CREATE or REPLACE Schema trustbankdata.analytics;
+
+SHOW SCHEMAS IN DATABASE trustbankdata;
